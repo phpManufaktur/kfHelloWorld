f7c62d3c7ed2311bb15df28b9891449d5e87d5e5
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-08-23 08:29:59 +0200

    added missing header information

--- bootstrap.include.php
+++ bootstrap.include.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * HelloWorld
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://kit2.phpmanufaktur.de
+ * @copyright 2012 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
 use thirdParty\HelloWorld\Control\HelloObject;
 use thirdParty\HelloWorld\Control\HelloBasic;
 use phpManufaktur\Basic\Control\kitCommand\Basic as kitCommandBasic;
